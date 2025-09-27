ec82de02734b8232dc5cbd14b0faff0dda26b5af
NeutrinoZh <neutrinozh@gmail.com>
2025-09-27 17:45:09 +0300

    implement visitor dialog window

--- Assets/_Project/Scripts/Core/Visitors/Visitor.cs
+++ Assets/_Project/Scripts/Core/Visitors/Visitor.cs
@@ -1,3 +1,5 @@
+using _Project.Sporae.Core;
+
 using System.Collections;
 using UnityEngine;
 
@@ -17,8 +19,30 @@ namespace _Project
         [SerializeField] private Transform _appearPosition;
         [SerializeField] private Transform _disappearPosition;
 
+        [SerializeField] private MissionConfig _missionConfig;
         [SerializeField] private float _speed;
 
+        [SerializeField] private VisitorDialog _visitorDialog;
+        
+        private Interactable _interactable;
+
+        private void Awake()
+        {
+            _interactable = GetComponent<Interactable>();
+            _interactable.OnInteract += HandleInteract;
+        }
+
+        private void OnDestroy()
+        {
+            _interactable.OnInteract -= HandleInteract;
+        }
+
+        private void HandleInteract()
+        {
+            if (State == VisitorState.Waiting)
+                _visitorDialog.Show(_missionConfig);
+        }
+        
         public void Appear()
         {
             State = VisitorState.Animating;
---
+++ Assets/_Project/Scripts/Core/Visitors/VisitorDialog.cs
@@ -0,0 +1,51 @@
+﻿using _Project.Sporae.Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project
+{
+    public class VisitorDialog : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _titleLabel;
+        [SerializeField] private TextMeshProUGUI _descriptionLabel;
+        [SerializeField] private TextMeshProUGUI _goalsLabel;
+        
+        [SerializeField] private Button _acceptButton;
+        [SerializeField] private Button _rejectButton;
+
+        private MissionConfig _missionConfig;
+        private MissionManager _missionManager;
+        
+        private void Awake()
+        {
+            _missionManager = ServiceContainer.Instance.Get<MissionManager>();
+            
+            _acceptButton.onClick.AddListener(HandleAccept);
+            _rejectButton.onClick.AddListener(HandleReject);
+        }
+
+        private void HandleAccept()
+        {
+            _missionManager.Append(_missionConfig);
+            Hide();
+        }
+
+        private void HandleReject()
+        {
+            Hide();
+        }
+
+        public void Show(MissionConfig missionConfig)
+        {
+            gameObject.SetActive(true);
+            _missionConfig = missionConfig;
+            _titleLabel.text = missionConfig.Title;
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
